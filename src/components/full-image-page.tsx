696dbb7a24f50db1d7f810d9d14678c52d6ef33d
Colin Eckert <colineckert10@gmail.com>
2024-07-24 19:40:32 -0700

    improve modal styles

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -3,5 +3,18 @@ import { getImage } from "~/server/queries";
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
 
-  return <img className="w-96" src={image.url} alt={image.name} />;
+  return (
+    <div className="flex h-full w-full min-w-0">
+      <div className="flex flex-shrink items-center justify-center">
+        <img
+          className="flex-shrink object-contain"
+          src={image.url}
+          alt={image.name}
+        />
+      </div>
+      <div className="flex w-48 flex-shrink-0 flex-col border-l">
+        <div className="text-xl font-bold">{image.name}</div>
+      </div>
+    </div>
+  );
 }
