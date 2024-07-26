f7857c5c82e5047776708d4a611ace315ef9b38d
Colin Eckert <colineckert10@gmail.com>
2024-07-26 13:29:12 -0700

    upload svg

--- src/app/_components/simple-upload-button.tsx
+++ src/app/_components/simple-upload-button.tsx
@@ -29,6 +29,25 @@ const useUploadThingInputProps = (...args: Input) => {
   };
 };
 
+function UploadSvg() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="size-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
+      />
+    </svg>
+  );
+}
+
 export function SimpleUploadButton() {
   const router = useRouter();
   const { inputProps } = useUploadThingInputProps("imageUploader", {
@@ -39,7 +58,9 @@ export function SimpleUploadButton() {
 
   return (
     <div>
-      <label htmlFor="upload-input">Upload</label>
+      <label htmlFor="upload-input" className="cursor-pointer">
+        <UploadSvg />
+      </label>
       <input
         id="upload-input"
         type="file"
