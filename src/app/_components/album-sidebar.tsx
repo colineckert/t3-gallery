faec5fadf1737bd0b53b0d3068743a3c19d000a7
Colin Eckert <colineckert10@gmail.com>
2024-08-26 14:26:47 -0700

    init create album modal UI

--- src/app/_components/album-sidebar.tsx
+++ src/app/_components/album-sidebar.tsx
@@ -1,5 +1,5 @@
 import { auth } from "@clerk/nextjs/server";
-import { FolderPlus } from "lucide-react";
+import { CreateAlbumModal } from "~/components/create-album-modal";
 import { Button } from "~/components/ui/button";
 import { getMyAlbums } from "~/server/queries";
 
@@ -19,10 +19,7 @@ export async function AlbumSidebar() {
     <div className="z-20 flex w-fit flex-col border-r bg-slate-950 p-4">
       <div className="mb-4 text-lg font-semibold">Albums</div>
       <div className="flex flex-row gap-4">
-        <Button variant="outline">
-          <FolderPlus className="mr-2 h-4 w-4" />
-          Create Album
-        </Button>
+        <CreateAlbumModal />
       </div>
       <div className="flex flex-col gap-2 py-3">
         {albums.map((album) => (
---
+++ src/components/create-album-modal.tsx
@@ -0,0 +1,45 @@
+import { FolderPlus } from "lucide-react";
+import { Button } from "~/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "~/components/ui/dialog";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
+
+export function CreateAlbumModal() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">
+          <FolderPlus className="mr-2 h-4 w-4" />
+          Create Album
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Create Album</DialogTitle>
+          <DialogDescription>
+            Create a new album to organize your images
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Name
+            </Label>
+            <Input id="name" defaultValue="Sick Pics" className="col-span-3" />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
