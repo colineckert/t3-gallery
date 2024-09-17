8884a0b259972ace6c3f00d9941e72ecd2866da4
Colin Eckert <colineckert10@gmail.com>
2024-09-16 21:03:55 -0700

    formatting

--- src/providers/gallery-store-provider.tsx
+++ src/providers/gallery-store-provider.tsx
@@ -1,47 +1,47 @@
-'use client'
+"use client";
 
-import { type ReactNode, createContext, useRef, useContext } from 'react'
-import { useStore } from 'zustand'
+import { type ReactNode, createContext, useRef, useContext } from "react";
+import { useStore } from "zustand";
 
 import {
   type GalleryStore,
   createGalleryStore,
   initGalleryStore,
-} from '~/stores/gallery-store'
+} from "~/stores/gallery-store";
 
-export type GalleryStoreApi = ReturnType<typeof createGalleryStore>
+export type GalleryStoreApi = ReturnType<typeof createGalleryStore>;
 
 export const GalleryStoreContext = createContext<GalleryStoreApi | undefined>(
   undefined,
-)
+);
 
 export interface GalleryStoreProviderProps {
-  children: ReactNode
+  children: ReactNode;
 }
 
 export const GalleryStoreProvider = ({
   children,
 }: GalleryStoreProviderProps) => {
-  const storeRef = useRef<GalleryStoreApi>()
+  const storeRef = useRef<GalleryStoreApi>();
   if (!storeRef.current) {
-    storeRef.current = createGalleryStore(initGalleryStore())
+    storeRef.current = createGalleryStore(initGalleryStore());
   }
 
   return (
     <GalleryStoreContext.Provider value={storeRef.current}>
       {children}
     </GalleryStoreContext.Provider>
-  )
-}
+  );
+};
 
 export const useGalleryStore = <T,>(
   selector: (store: GalleryStore) => T,
 ): T => {
-  const galleryStoreContext = useContext(GalleryStoreContext)
+  const galleryStoreContext = useContext(GalleryStoreContext);
 
   if (!galleryStoreContext) {
-    throw new Error('useGalleryStore must be used within GalleryStoreProvider')
+    throw new Error("useGalleryStore must be used within GalleryStoreProvider");
   }
 
-  return useStore(galleryStoreContext, selector)
-}
+  return useStore(galleryStoreContext, selector);
+};
